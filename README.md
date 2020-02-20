8ea0b95921b3d58cfb2e9d3346d63b834b89c2e2
Don Moses <githinjidon@gmail.com>
2020-02-20 10:54:48 +0300

    add license, readme and manage.py

---
+++ README.md
@@ -0,0 +1,18 @@
+# Blogging
+#### This is a website built on the flask framework that displays blog posts by different users. 20/2/2020. 
+#### By **Don Moses**
+## Description
+On loading up the website, the user is provided with a list of blog posts previously posted. On login, a user can add new blog posts as well as comment on other user's posts.Without an existing user account, the user can register and then login afterwards.
+## Setup/Installation Requirements
+Users may view a working demo here: 
+## Known Bugs
+
+## Technologies Used
+For development, the author used pycharm to develop using html, css and python3.6. Furthermore, flask dependencies were used
+## Codebeat Badge
+
+## Support and contact details
+In case of any problems, please email me at githinjidon@gmail.com
+
+## License
+[LICENSE](license)
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,78 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, EqualTo
-from wtforms import ValidationError
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from . import auth
+from .. import db
 from ..models import User
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.endpoint[:5] != 'auth.' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
-class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
-    username = StringField('Username', validators=[
-        Required(), Length(1, 64)])
-    password = PasswordField('Password', validators=[
-        Required(), EqualTo('password2', message='Passwords must match')])
-    password2 = PasswordField('Confirm Password', validators=[Required()])
-    submit = SubmitField('Register')
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
 
-    def validate_username(self, field):
-        if User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username already in use.')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                    'auth/email/confirm', user=user, token=token)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+                'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email')
+    return redirect(url_for('main.index'))
