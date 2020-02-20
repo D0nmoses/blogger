b7170ad75878233efd3194fd584478679f3ac60e
Don Moses <githinjidon@gmail.com>
2020-02-20 11:37:59 +0300

    add role model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,14 @@ from flask import current_app, request, url_for
 from flask_login import UserMixin
 from . import db, login_manager
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
