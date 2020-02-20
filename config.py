6d797319faa874efc1d4b23258399d995f177cd6
Don Moses <githinjidon@gmail.com>
2020-02-20 16:57:20 +0300

    add 404.html to templates

--- config.py
+++ config.py
@@ -13,13 +13,6 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # FLASKY_MAIL_SUBJECT_PREFIX = '[Blogging]'
-    # FLASKY_MAIL_SENDER = 'Blogging Admin <blogging@demo.com>'
-    # FLASKY_ADMIN = os.environ.get('BLOGGING_ADMIN')
-    # FLASKY_POSTS_PER_PAGE = 20
-    # FLASKY_FOLLOWERS_PER_PAGE = 50
-    # FLASKY_COMMENTS_PER_PAGE = 30
-    # FLASKY_SLOW_DB_QUERY_TIME=0.5
 
     @staticmethod
     def init_app(app):
